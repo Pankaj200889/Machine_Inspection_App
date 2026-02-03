1b190d3eec23d82b3a96b1d701a5aac8343aa19e
Pankaj Vishwakarma <vishwapa@outlook.com>
2026-02-03 16:50:13 +0530

    Add seed_operator script

---
+++ server/seed_operator.js
@@ -0,0 +1,35 @@
+const db = require('./database');
+const bcrypt = require('bcryptjs');
+
+async function seedOperator() {
+    console.log("Seeding Operator...");
+    const username = 'operator';
+    const email = 'operator@example.com';
+    const password = 'operator123';
+
+    // Check if exists
+    const check = await db.query("SELECT * FROM users WHERE username = ?", [username]);
+    if (check.rows.length > 0) {
+        console.log("Operator already exists. Updating password...");
+        // Update password just in case
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        await db.query("UPDATE users SET password_hash = ? WHERE id = ?", [hash, check.rows[0].id]);
+        console.log("Password updated to 'operator123'");
+    } else {
+        console.log("Creating new operator...");
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        await db.query("INSERT INTO users (username, email, password_hash, role) VALUES (?, ?, ?, ?)",
+            [username, email, hash, 'operator']);
+        console.log("Operator created.");
+    }
+}
+
+seedOperator().then(() => {
+    console.log("Done");
+    process.exit(0);
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
+});

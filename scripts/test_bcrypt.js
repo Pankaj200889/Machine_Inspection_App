c8a1a3b9916092bc0fb0dc34de1fb4061929130c
Pankaj Vishwakarma <vishwapa@outlook.com>
2026-08-14 13:19:55 +0530

    cleanup: remove temporary test scripts

--- scripts/test_bcrypt.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('../server/node_modules/bcryptjs');
-const hash = '$2b$10$r8ENIRSJPEUB13ztb4U.Z.jLqhbaJvMGraDL.1sJr4cjjlvmNC/b2';
-const password = 'Admin@124578';
-console.log("Match:", bcrypt.compareSync(password, hash));
